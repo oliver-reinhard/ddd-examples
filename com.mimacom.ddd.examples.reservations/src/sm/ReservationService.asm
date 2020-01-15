e0eeef26d0d93416b0cb2162a1fa5924aa8f328a
Oliver Reinhard <oliver.reinhard@gmail.com>
2020-01-15 13:23:17 +0100

    Add Permits example.
    - Fix issues in Reservations example.

--- com.mimacom.ddd.examples.reservations/src/sm/ReservationService.asm
+++ com.mimacom.ddd.examples.reservations/src/sm/ReservationService.asm
@@ -7,20 +7,23 @@ service interface sm.UIService
 interface model sm.ReservationsUI
 core model 		sm.ReservationsCore
 
+
 operation CreateParty(
 	in name : Name, in size : Size, out p : SParty
 ) { 
-	effect p = new SParty(newPartyID, name, size)
+	effect p = SParty(newPartyID, name, size)
 } 
+
 
 operation PartyArrived(
 	in p : SParty, /* Workaround */ in t : STable
 ) {
 	guard	NOT allTables.exists(t.party.id = p.id) 
 	guard	NOT allWaitingParties.exists(p)
-	effect 	allTables.exists(t.party = p AND t.state = TableState.USED)
+	effect 	allTables.exists(party = p AND state = Table.USED)
 	effect 	allWaitingParties.exists(p)
 } 
+
 
 operation TableCleaned(
 	in t : STable
